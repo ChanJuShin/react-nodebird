460e9f22dd14fedef3e6f04b154ae138539333ae
ChanJuShin <ckswn1259@naver.com>
2021-02-02 21:20:42 +0900

    AppyLayout 반응형 그리드 추가, LoginForm 퍼블리싱

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
 
 const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>
             <Menu mode="horizontal">
@@ -14,10 +17,23 @@ const AppLayout = ({ children }) => {
                     <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
                 <Menu.Item>
+                    <Input.Search enterButton style={{ verticalAlign: "middle" }}  />
+                </Menu.Item>
+                <Menu.Item>
                     <Link href="/signup"><a>회원가입</a></Link>
                 </Menu.Item>
             </Menu>
-            {children}
+            <Row gutter={8}>
+                <Col xs={24} sm={6} md={6}>
+                    {isLoggedIn ? <UserProfile /> : <LoginForm /> }
+                </Col>
+                <Col xs={24} sm={12} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} sm={6} md={6}>
+                    <a href="https://www.zerocho.com" target="_blank" rel="noreferrer noopener">Made by Zerocho</a>
+                </Col>
+            </Row>
         </div>
     );
 };
