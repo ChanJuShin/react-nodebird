a9b3508a70b1a9cd949acd8866a933aa209aa964
ChanJuShin <ckswn1259@naver.com>
2021-02-16 12:41:50 +0900

    faker로 더미포스트 처리 및 미로그인 상태에서 프로필 페이지 접속시 메인으로 리다이렉트 처리

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
+
+  const router = useRouter();
+  useEffect(() => {
+    if (!(me && me.id)) {
+      router.push('/');
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
 
   return (
     <>
