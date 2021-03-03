81b7d8a6bf8affc6d16c6b66f1299dd8df46a2ac
ChanJuShin <ckswn1259@naver.com>
2021-03-03 12:29:32 +0900

    sequelize 최신문법화

--- back/models/image.js
+++ back/models/image.js
@@ -1,19 +1,26 @@
-module.exports = (sequelize, DataTypes) => {
-  const Image = sequelize.define(
-    'Image',
-    {
-      src: {
-        type: DataTypes.STRING(200),
-        allowNull: false
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Image extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        // id가 기본적으로 들어있다.
+        src: {
+          type: DataTypes.STRING(200),
+          allowNull: false
+        }
+      },
+      {
+        modelName: 'Image',
+        tableName: 'images',
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        sequelize
       }
-    },
-    {
-      charset: 'utf8',
-      collate: 'utf8_general_ci'
-    }
-  );
-  Image.associate = (db) => {
+    );
+  }
+  static associate(db) {
     db.Image.belongsTo(db.Post);
-  };
-  return Image;
+  }
 };
--- back/models/user.js
+++ back/models/user.js
@@ -1,27 +1,35 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      email: {
-        type: DataTypes.STRING(30),
-        allowNull: false,
-        unique: true
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        // id가 기본적으로 들어있다.
+        email: {
+          type: DataTypes.STRING(30), // STRING, TEXT, BOOLEAN, INTEGER, FLOAT, DATETIME
+          allowNull: false, // 필수
+          unique: true // 고유한 값
+        },
+        nickname: {
+          type: DataTypes.STRING(30),
+          allowNull: false // 필수
+        },
+        password: {
+          type: DataTypes.STRING(100),
+          allowNull: false // 필수
+        }
       },
-      nickname: {
-        type: DataTypes.STRING(30),
-        allowNull: false
-      },
-      password: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+      {
+        modelName: 'User',
+        tableName: 'users',
+        charset: 'utf8',
+        collate: 'utf8_general_ci', // 한글 저장
+        sequelize
       }
-    },
-    {
-      charset: 'utf8',
-      collate: 'utf8_general_ci'
-    }
-  );
-  User.associate = (db) => {
+    );
+  }
+  static associate(db) {
     db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
     db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
@@ -35,6 +43,5 @@ module.exports = (sequelize, DataTypes) => {
       as: 'Followings',
       foreignKey: 'FollowerId'
     });
-  };
-  return User;
+  }
 };
