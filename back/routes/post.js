0790894b6b8030b432b8d67270afad88e279205c
ChanJuShin <ckswn1259@naver.com>
2021-03-13 00:02:55 +0900

    multer AWS S3 기능으로 변경

--- back/routes/post.js
+++ back/routes/post.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 
 const { Post, Image, Comment, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
@@ -15,15 +17,17 @@ try {
   fs.mkdirSync('uploads');
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2'
+});
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'uploads');
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname);
-      const basename = path.basename(file.originalname, ext);
-      done(null, basename + '_' + new Date().getTime() + ext);
+  storage: multerS3({
+    storage: new AWS.S3(),
+    bucket: 'mcscj',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     }
   }),
   limits: { fileSize: 20 * 1024 * 1024 }
@@ -162,7 +166,7 @@ router.get('/:postId', async (req, res, next) => {
 });
 
 router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => {
-  res.json(req.files.map((v) => v.filename));
+  res.json(req.files.map((v) => v.location));
 });
 
 router.post('/:postId/comment', isLoggedIn, async (req, res, next) => {
