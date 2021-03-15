a1a5c61333a7ad7c1db243536a6e579641d66b1a
ChanJuShin <ckswn1259@naver.com>
2021-03-15 22:43:44 +0900

    이미지 리사이즈 람다 오류 수정

--- lambda/index.js
+++ lambda/index.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
   const Key = decodeURIComponent(event.Records[0].s3.object.key);
   console.log(Bucket, Key);
-  const filename = Key.split("/")[key.split("/").length - 1];
+  const filename = Key.split("/")[Key.split("/").length - 1];
   const ext = Key.split(".")[Key.split(".").length - 1].toLowerCase();
   const requiredFormat = ext === "jpg" ? "jpeg" : ext;
   console.log("filename ::: ", filename, " ext ::: ", ext);
