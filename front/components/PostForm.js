1d028d4db10f16ad154e22613aeab6face574034
ChanJuShin <ckswn1259@naver.com>
2021-03-15 23:43:16 +0900

    게시글 폼 이미지 리사이즈 후 오리지날 경로로 읽게 변경 커밋본 잘못 커밋해서 수정

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -93,11 +93,12 @@ const PostForm = () => {
       </div>
       <div>
         {imagePaths.map((v, i) => (
-          <div
-            key={v.replace(/\/thumb\//, '/original/')}
-            style={{ display: 'inline-block' }}
-          >
-            <img src={`${v}`} style={{ width: '200px' }} alt={v} />
+          <div key={v} style={{ display: 'inline-block' }}>
+            <img
+              src={v.replace(/\/thumb\//, '/original/')}
+              style={{ width: '200px' }}
+              alt={v}
+            />
             <div>
               <Button onClick={onRemoveImage(i)}>제거</Button>
             </div>
